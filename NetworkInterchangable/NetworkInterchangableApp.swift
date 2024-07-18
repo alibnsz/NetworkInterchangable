0a18bf8c3ba142528056cd37f06b886bcf0785c3
Mehmet Ali Bunsuz <m.bunsuz1@gmail.com>
2024-07-18 15:00:40 +0300

    Initial Commit

---
+++ NetworkInterchangable/NetworkInterchangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangableApp.swift
+//  NetworkInterchangable
+//
+//  Created by Mehmet Ali Bunsuz on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
